143bb0fd58981a27fba4b427559eb906bbbabae9
alexsylvian <lazeratus1@gmail.com>
2023-08-19 22:44:06 -0400

    added booklist

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener('DOMContentLoaded', () => {
+    const bookList = document.getElementById('list')
+
+    function renderList(){
+        fetch('http://localhost:3000/books')
+        .then(res => res.json())
+        .then(books => {
+            books.forEach(book => {
+                const bookLine = document.createElement('li')
+                bookLine.innerText = book.title
+                bookList.appendChild(bookLine)
+            })
+        })
+    }
+
+    renderList()
+})
